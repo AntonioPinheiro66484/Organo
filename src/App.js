17bd65859f044c8ab491576af278c80a85ea718d
HadeSatanist <ap66484@gmail.com>
2023-01-01 16:50:41 -0300

    Insersão de Componentes no App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Banner from './components/Banner/Banner';
-
+import Banner from './components/Banner';
+import Formulario from './components/Formulario';
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Formulario/>      
     </div>
   );
 }
